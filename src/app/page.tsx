b14c16cc2bc98eb3fffb3521462e6245a84af961
Tarunna Sen <tarunnasen@gmail.com>
2026-07-15 03:03:12 +0600

    About US Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import AboutUs from "@/components/AboutUS";
 import WhyChooseUS from "@/components/WhyChooseUS";
 
 export default function Home() {
   return (
     <>
+      <AboutUs />
+      <div className="divider"></div>
       <WhyChooseUS />
     </>
   );
---
+++ src/components/AboutUS.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import SectionContainer from "./SectionContainer";
+
+export default function AboutUs() {
+    return (
+      <SectionContainer
+        title="About US"
+        subtitle="What is GameZone and What do We offer?"
+      >
+        <div className="flex flex-row items-center justify-center gap-10">
+          <Image
+            src="https://image.benq.com/is/image/benqco/Gamingroom_tk700sti-A?$ResponsivePreset$"
+            alt="Game Room"
+            width={600}
+            height={300}
+          />
+          <div>
+            <h1 className="text-4xl font-bold mb-8">What is GameZone?</h1>
+            <p className="text-lg text-justify mb-12">Gamezone is a physical shop in Chawkbazar, Chattogram where you can come and  play different Play Station 4 and Play Station 5 games with your friends and fmily with an very cheap hourly rate. We offer various range of gameslike Cricket, Football along with very cheap hourly rate and 24/7 air conditioning.</p>
+            <p className="text-lg font-bold mb-3">Shop Time List</p>
+            <ul className="list-disc ml-4">
+                <li>Sunday - Thursday: 10:00 a.m. - 08 p.m.</li>
+                <li>Friday - Satuday: 03:00 p.m. - 11 p.m.</li>
+            </ul>
+          </div>
+        </div>
+      </SectionContainer>
+    );
+}
