b78cea01b10d5ad8b990408fd35b2464588d7f74
Tarunna Sen <tarunnasen@gmail.com>
2026-07-15 17:41:14 +0600

    Solved implicitly anytype error

--- src/lib/fetchGames.ts
+++ src/lib/fetchGames.ts
@@ -1,4 +1,16 @@
-export async function fetchGames(id? :string ) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/games/${id? id : ""}`);
+export interface Game {
+  _id: string;
+  gameName: string;
+  gameCategory: string;
+  gameShortDescription: string;
+  gameDescription: string;
+  gameImageURL: string;
+  hourlyRate: number;
+}
+
+export async function fetchGames(id?: string): Promise<Game[]> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/games/${id ? id : ""}`,
+  );
   return res.json();
-};
+}
