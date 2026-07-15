547da6af46a360894fd47987f8a4e2f1a20efb60
Tarunna Sen <tarunnasen@gmail.com>
2026-07-15 22:04:05 +0600

    Login Functionality

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,86 @@
+"use client";
+
 import Button from "@/components/Buttons";
 import InputFieldLabel from "@/components/InputFieldLabel";
 import SectionContainer from "@/components/SectionContainer";
+import { authClient } from "@/lib/auth-client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { error } = await authClient.signIn.email({
+      ...data,
+      rememberMe: true,
+      callbackURL: "/",
+    });
+
+    if (!error) {
+      toast.success("Welcome Back!");
+      reset();
+    } else {
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <SectionContainer title="Login" subtitle="Login to book your slots and manage your bookings">
+    <SectionContainer
+      title="Login"
+      subtitle="Login to book your slots and manage your bookings"
+    >
       <div className="flex mx-auto max-w-5xl items-center justify-center">
-        <form className="w-full rounded-box border border-gray-600 p-8">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full rounded-box border border-gray-600 p-8"
+        >
           <div className="flex flex-col gap-2 mb-6">
-            <InputFieldLabel text="Email Address" required/>
+            <InputFieldLabel text="Email Address" required />
             <input
               type="email"
               className="input text-black w-full"
               placeholder="john.doe@example.com"
+              {...register("email", {
+                required: "Email is required",
+              })}
             />
+            {errors.email && (
+              <span className="text-sm text-red-500 font-medium">
+                {errors.email.message}
+              </span>
+            )}
           </div>
 
           <div className="flex flex-col gap-2 mb-6">
-            <InputFieldLabel text="Password" required/>
+            <InputFieldLabel text="Password" required />
             <input
               type="password"
               className="input text-black w-full"
               placeholder="Enter your password"
+              {...register("password", {
+                required: "Password is required",
+              })}
             />
+            {errors.password && (
+              <span className="text-sm text-red-500 font-medium">
+                {errors.password.message}
+              </span>
+            )}
           </div>
 
           <div className="flex items-center justify-center gap-4">
-            <Button text="Login" type="primary" task="button" />
+            <Button text="Login" type="primary" btnType="submit" task="button" />
             <Button text="Clear" type="secondary" task="button" />
           </div>
         </form>
