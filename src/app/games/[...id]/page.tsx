9ea20339ba33ccb30d0b71b51eba251099bbf589
Tarunna Sen <tarunnasen@gmail.com>
2026-07-15 17:05:57 +0600

    C(R)UD - Game Details Page

--- src/app/games/[...id]/page.tsx
+++ src/app/games/[...id]/page.tsx
@@ -1,20 +1,27 @@
 import SectionContainer from "@/components/SectionContainer";
+import { fetchGames } from "@/lib/fetchGames";
 import Image from "next/image";
 
-export default function GameDetails() {
-  const game = {
-    id: 1,
-    title: "EA SPORTS FC 26",
-    category: "Football",
-    description:
-      "The latest football simulation featuring authentic clubs, leagues, and players.",
-    longDescription:
-      "EA SPORTS FC 26 delivers the most realistic football experience with over 20,000 licensed players, hundreds of clubs, and official leagues from around the world. Enjoy Career Mode, Ultimate Team, Clubs, and Kick-Off with improved gameplay, smarter AI, and enhanced graphics. Whether you're competing online or building your dream squad, FC 26 offers countless hours of football action.",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWKxANvsGXXFA-NTnHRAKaXjd38qcfWUBU-05QAYkwcDzFsyAz6uG0S-7w&s=10",
-    hourlyRate: 10.0,
-  };
-  
+interface GameDetailsProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function GameDetails({ params }: GameDetailsProps) {
+  const { id } = await params;
+  const game = await fetchGames(id);
+
+  if (game.length === 0) {
+    return (
+      <div className="flex items-center justify-center">
+        <span className="loading loading-spinner loading-md"></span>
+      </div>
+    );
+  }
+
+  const imageSize: number = 500;
+
   return (
     <SectionContainer
       title="Game Details"
@@ -23,10 +30,10 @@ export default function GameDetails() {
       <div className="flex flex-col items-center justify-center md:flex-row gap-16">
         <div className="md:w-1/3">
           <Image
-            src={game.imageUrl}
-            alt={game.title}
-            width={500}
-            height={500}
+            src={game.gameImageURL}
+            alt={game.gameName}
+            width={imageSize}
+            height={imageSize}
             className="w-full h-auto rounded-lg shadow-md"
           />
         </div>
@@ -34,18 +41,18 @@ export default function GameDetails() {
           <h4 className="mb-4">
             Category:{" "}
             <span className="text-blue-500 font-semibold text-xl">
-              {game.category}
+              {game.gameCategory}
             </span>
           </h4>
-          <h2 className="text-6xl font-bold mb-4">{game.title}</h2>
-          <p className="text-sm">{game.description}</p>
-          <p className="my-12 text-lg text-justify">{game.longDescription}</p>
+          <h2 className="text-6xl font-bold mb-4">{game.gameName}</h2>
+          <p className="text-sm">{game.gameShortDescription}</p>
+          <p className="my-12 text-lg text-justify">{game.gameDescription}</p>
 
           <div className="flex flex-col gap-2 text-sm">
             <p>Playing Rent: </p>
             <p>
               <span className="text-4xl font-bold text-blue-500">
-                ${game.hourlyRate.toFixed(2)}
+                {game.hourlyRate}
               </span>{" "}
               / hour
             </p>
--- src/lib/fetchGames.ts
+++ src/lib/fetchGames.ts
@@ -1,14 +1,4 @@
-export interface Game {
-  _id: string;
-  gameName: string;
-  gameCategory: string;
-  gameShortDescription: string;
-  gameDescription: string;
-  gameImageURL: string;
-  hourlyRate: number;
-}
-
-export async function fetchGames(): Promise<Game[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/games`);
+export async function fetchGames(id? :string ) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/games/${id? id : ""}`);
   return res.json();
 };
